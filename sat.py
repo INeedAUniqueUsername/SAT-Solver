24bcf20c1daef1fdef5dd8e394e80b026a165419
Alex "Archcannon" Chen <achen115@ucr.edu>
2022-06-28 01:04:19 -0700

    Create sat.py

---
+++ sat.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+print("SAT Solver 2022 (by INeedAUniqueUsername)")
+print("**For educational purposes ONLY**")
+for i in range(52):
+  time.sleep(60)
+  print("B")
+
+for i in range(44):
+  time.sleep(60)
+  print(["A", "B", "C", "D"][random.randint()%4])
+  
+for i in range(58):
+  time.sleep(60)
+  print("B")
+  
+print("THANK GOD IT'S OVER")
